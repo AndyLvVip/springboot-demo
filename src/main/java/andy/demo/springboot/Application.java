2989ff7caa178643a3af7d9d9048c49ad6ccda18
andy.lv <andy.lv.vip@hotmail.com>
2018-01-06 10:07:25 +0800

    create project...

---
+++ src/main/java/andy/demo/springboot/Application.java
@@ -0,0 +1,14 @@
+package andy.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"andy.demo.springboot"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
